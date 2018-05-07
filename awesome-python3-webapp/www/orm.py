6e80fe83aec0d2de73c8b3eeebd5903be9eb86e2
guanwenyao <guanwenyao@chinamobile.com>
2018-05-07 17:44:57 +0800

    May 7, 2018

--- awesome-python3-webapp/www/orm.py
+++ awesome-python3-webapp/www/orm.py
@@ -116,25 +116,25 @@ class ModelMetaclass(type):
                 if v.primary_key:
                     # Find the primary key:
                     if primaryKey:
-                        raise StandardError('Duplicate primary key for field: %s' % k)
+                        raise RuntimeError('Duplicate primary key for field: %s' % k)
                     primaryKey = k
                 else:
                     fields.append(k)
         if not primaryKey:
-            raise StandardError('Primary key not found.')
+            raise RuntimeError('Primary key not found.')
 
         for k in mappings.keys():
             attrs.pop(k)
 
         escaped_fields = list(map(lambda f: '`%s`' % f, fields))
-        attrs[''] = mappings
-        attrs[''] = tablename
-        attrs[''] = primaryKey
-        attrs[''] = fields
-        attrs[''] = 'select `%s`, %s from `%s`' % (primaryKey, ', '.join(escaped_fields), tablename)
-        attrs[''] = 'insert into `%s` (%s, `%s`) values (%s)' (tablename, ', '.join(escaped_fields), primaryKey, create_args_string(len(escaped_fields) + 1))
-        attrs[''] = 'update `%s` set %s where `%s` = ?' % (tablename, ', '.join(map(lambda f: '`%s`=?' % (mappings.get(f).name or f), fields)), primaryKey)
-        attrs[''] = 'delete from `%s` where `%s`=?' % (tablename, primaryKey)
+        attrs['__mappings__'] = mappings
+        attrs['__table__'] = tablename
+        attrs['__primary_key__'] = primaryKey
+        attrs['__fields__'] = fields
+        attrs['__select__'] = 'select `%s`, %s from `%s`' % (primaryKey, ', '.join(escaped_fields), tablename)
+        attrs['__insert__'] = 'insert into `%s` (%s, `%s`) values (%s)' (tablename, ', '.join(escaped_fields), primaryKey, create_args_string(len(escaped_fields) + 1))
+        attrs['__update__'] = 'update `%s` set %s where `%s` = ?' % (tablename, ', '.join(map(lambda f: '`%s`=?' % (mappings.get(f).name or f), fields)), primaryKey)
+        attrs['__delete__'] = 'delete from `%s` where `%s`=?' % (tablename, primaryKey)
 
         return type.__new__(cls, name, bases, attrs)
 
